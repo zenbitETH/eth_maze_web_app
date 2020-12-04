b2ce5a9684d3778964ea097d1b974e1ea55aaf27
mario <mario.briseno7@gmail.com>
2020-12-03 20:22:28 -0600

    Canvas component working without an specific maze

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,43 +1,54 @@
 import React, {useEffect, useState} from "react"
 import Web3 from "web3"
+import ItemsContainer from "../items/itemsContainer"
 import "../../style/index.css"
+import Land from "../land/land"
+import Controls from "../controls/controls"
+import Header from "./header"
 
 var contract = require("@truffle/contract");
 let web3;
 let instance;
 let RaribleContract;
-let image
+
 
 function Canvas() {
 
     const [buttonVisibility, setButtonVisibility] = useState("") 
     const [Rarible, setRarible] = useState(null)
     let [accounts, setAccounts] = useState([])
-
-    function fetchRaribleJSON() {
-        fetch("https://zenbit-util-contracts.s3.amazonaws.com/rarible.json",
-        {mode: 'cors'})
-        .then(res => res.json())
-        .then(result => {
-            setRarible(result)
-            console.log(Rarible)
-        })
-    }
+    let [_pos, setPosition] = useState("00")
 
     useEffect(() => {
+        initMetaMask()
+        web3.eth.getAccounts().then(accnts => {
+            setAccounts(accnts)
+        })
         fetchRaribleJSON()
-        console.log(Rarible)
     }, [])
 
     useEffect(() => {
         if(accounts.length === 0) {
             setButtonVisibility("initial")
         } else {
-            setButtonVisibility("none")
+            setButtonVisibility("hidden")
         }
     }, [accounts])
 
-    async function initComponent() {
+    const superiorSetPosition = _position => {
+        setPosition(_position)
+    }
+
+    const fetchRaribleJSON = () => {
+        fetch("https://zenbit-util-contracts.s3.amazonaws.com/rarible.json",
+        {mode: 'cors'})
+        .then(res => res.json())
+        .then(result => {
+            setRarible(result)
+        })
+    }
+
+    const initMetaMask = () => {
         if (window.ethereum) {
             web3 = new Web3(window.ethereum)
         }
@@ -50,9 +61,18 @@ function Canvas() {
             alert('You have to install MetaMask !');
         }
         if(typeof web3 === "undefined") {
-            return 0
+            return false
+        } else {
+            return true
         }
+    }
 
+    const initComponent = async () => {
+        
+        let walletExists = initMetaMask()
+        if (!walletExists) {
+            return null
+        } 
         web3.eth.getAccounts().then(accnts => {
             setAccounts(accnts)
         })
@@ -62,14 +82,13 @@ function Canvas() {
         instance = await RaribleContract.at(
             Rarible.address
         )
-        console.log(instance)
         let stuff = await instance.uri(77159, {from: accounts[0]})
-        console.log(stuff)
         let resp = await (await (await fetch(stuff)).json())
-        console.log(resp)
-    }
 
-    async function enableMetamask() {
+        return 
+    }
+                    
+    const enableMetamask = async () => {
         let _accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         setAccounts(_accounts)
         setButtonVisibility("none")
@@ -77,17 +96,17 @@ function Canvas() {
     }
 
     return(
-        <div>
-            <button type="submit"
-            onClick={enableMetamask}
-            style={{display: buttonVisibility}}
-            className="button-submit">
-                Conectar Metamask
-            </button>
-            <video 
-            src="https://gateway.ipfs.io/ipfs/QmZdHmq1LmhZBcZ3uF6or8Wp9CrNV2eXN1U4bqVMksbxUK/animation.mp4"
-            width="320" height="240" controls>
-            </video>
+        <div className="canvas">
+            <div className="canvas-header" style={{overflow: buttonVisibility}}>
+                <Header 
+                args={buttonVisibility} 
+                enableMetamask={enableMetamask}/>
+            </div>
+            <div className="playground">
+                <Controls position={_pos} changePosition={superiorSetPosition}/>
+                <Land len={8} position={_pos} />
+            </div>
+            <ItemsContainer />
         </div>
     )
 }
