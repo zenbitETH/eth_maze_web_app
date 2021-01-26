a0ee37e0a4138fa405c9da7afb276607e8ace032
mario <mario.briseno7@gmail.com>
2021-01-25 20:10:53 -0600

    src/components/header/header.js

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Header() {
+    return(
+        <div className="app-header">
+            <img className="logo" src="aztec-logo.png"></img>
+        </div>
+    )
+}
+
+export default Header
