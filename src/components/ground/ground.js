c7995be712fbe06dcfe3818a3ff59d785d9599b6
mario <mario.briseno7@gmail.com>
2021-02-07 01:43:35 -0600

    assets used in the game

--- src/components/ground/ground.js
+++ src/components/ground/ground.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React, {useEffect} from "react"
+import AddressSearch from "./addressSearch"
 
+function Ground(props) {
 
-function Ground() {
+    useEffect(() => {
+        console.log(props.position)
+    }, [props.position])
+
+    const componentRender = () => {
+        let component;
+        if(props.position == 32){
+            component = <AddressSearch changeAllowance={props.changeAllowance}/>
+        }
+        return component
+    }
+
     return(
         <div className="ground">
-
+            {componentRender()}
         </div>
     )
 }
