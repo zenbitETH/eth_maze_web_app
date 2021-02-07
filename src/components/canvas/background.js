f9b22e77b09e91ccb79af806abd5e6d1277df62c
mario <mario.briseno7@gmail.com>
2021-02-06 20:56:49 -0600

    exporting setBackgroundImage to allow other components calling this function

--- src/components/canvas/background.js
+++ src/components/canvas/background.js
@@ -3,16 +3,15 @@ var url = "https://zenbit-util-contracts.s3.amazonaws.com/fondo"
 
 const setBackgroundImage = url => {
     let ele = document.getElementById("body")
-    console.log(url)
     ele.style.backgroundImage = `url(${url})`
 }
 
 const changeBodyStyle = position => {
-    let image = "A.jpg"
+    let image = "B.jpg"
     if(position == 34 || position == 35) {
         image = "Z.jpg"
     }
     setBackgroundImage(url+image)
 }
 
-export {changeBodyStyle, url}
+export {changeBodyStyle, setBackgroundImage, url}
