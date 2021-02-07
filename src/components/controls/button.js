441321001e929aa35e3a445014632648ecc8ea54
mario <mario.briseno7@gmail.com>
2021-02-07 01:53:27 -0600

    allowing buttons to be hidden and to change clases

--- src/components/controls/button.js
+++ src/components/controls/button.js
@@ -4,8 +4,10 @@ function Button(props) {
     return(
         <button 
             id={props.id}
-            onClick={props.data.move}
-            className="button-maze">
+            onClick={props.data.fireFunction}
+            className={"button-maze "+props.spec}
+            style={{visibility: props.isHiden ? "hidden": "visible"}}
+        >
             {props.data.message}
         </button>
     )
