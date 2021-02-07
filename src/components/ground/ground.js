9d9835294e172eb7b8f804fbc8185c80735393d0
mario <mario.briseno7@gmail.com>
2021-02-07 01:49:55 -0600

    created conditional render for ground component for the change of positions

--- src/components/ground/ground.js
+++ src/components/ground/ground.js
@@ -1,22 +1,35 @@
 import React, {useEffect} from "react"
+import SnakeAttack from "./snakeAttack"
 import AddressSearch from "./addressSearch"
+import FirstLevelBoss from "./firstLevelBoss"
 
 function Ground(props) {
 
     useEffect(() => {
-        console.log(props.position)
     }, [props.position])
 
     const componentRender = () => {
         let component;
         if(props.position == 32){
             component = <AddressSearch changeAllowance={props.changeAllowance}/>
+        } else if(props.position == 35) {
+            component = <SnakeAttack changeAllowance={props.changeAllowance} /> 
+        } else if(props.position == 36) {
+            component = <FirstLevelBoss changeAllowance={props.changeAllowance} /> 
         }
         return component
     }
 
     return(
-        <div className="ground">
+        <div 
+            className="ground" 
+            id="ground"
+            style={
+                {
+                    visibility: props.position == "35" ? "hidden": "visible"
+                }
+            }    
+        >
             {componentRender()}
         </div>
     )
